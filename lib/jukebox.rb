14681f7e09bfdff9206054cbf5c417863e25e653
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-27 16:15:58 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,6 +1,5 @@
 # Add your code here
 def help()
-  help = <<-HELP
   puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
